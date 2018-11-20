24771d3605137d23136dfc3ab3598c7201dd2068
yuan310979 <chc71340@gmail.com>
2018-11-20 16:35:42 +0800

    correct the identation

--- hw03/integrate.c
+++ hw03/integrate.c
@@ -5,24 +5,24 @@
 
 int main(int argc, char **argv) 
 {
-  long long i, num_intervals;
-  double rect_width, area, sum, x_middle; 
+    long long i, num_intervals;
+    double rect_width, area, sum, x_middle; 
 
-  sscanf(argv[1],"%llu",&num_intervals);
+    sscanf(argv[1],"%llu",&num_intervals);
 
-  rect_width = PI / num_intervals;
+    rect_width = PI / num_intervals;
 
-  sum = 0;
-  for(i = 1; i < num_intervals + 1; i++) {
+    sum = 0;
+    for(i = 1; i < num_intervals + 1; i++) {
 
-    /* find the middle of the interval on the X-axis. */ 
+        /* find the middle of the interval on the X-axis. */ 
 
-    x_middle = (i - 0.5) * rect_width;
-    area = sin(x_middle) * rect_width; 
-    sum = sum + area;
-  } 
+        x_middle = (i - 0.5) * rect_width;
+        area = sin(x_middle) * rect_width; 
+        sum = sum + area;
+    } 
 
-  printf("The total area is: %f\n", (float)sum);
+    printf("The total area is: %f\n", (float)sum);
 
-  return 0;
+    return 0;
 }   
