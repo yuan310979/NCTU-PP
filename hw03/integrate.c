4f70266c36fd590d4257524d4aa2a85dc493bd5d
yuan310979 <chc71340@gmail.com>
2018-11-21 21:32:29 +0800

    integrate.c

--- hw03/integrate.c
+++ hw03/integrate.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 #include <math.h>
+#include <mpi.h>
 
 #define PI 3.1415926535
 
 int main(int argc, char **argv) 
 {
     long long i, num_intervals;
+    int my_rank, p;
     double rect_width, area, sum, x_middle; 
+
+    /* start up MPI */
+    MPI_Init(&argc, &argv);
+
+    /* find out process rank */
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    /* find out number of process */
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
     sscanf(argv[1],"%llu",&num_intervals);
 
     rect_width = PI / num_intervals;
 
     sum = 0;
-    for(i = 1; i < num_intervals + 1; i++) {
+    double local_sum = 0;
 
-        /* find the middle of the interval on the X-axis. */ 
+    long long start = (long long)(num_intervals * (float)(my_rank)/p) + 1;
+    long long end = (long long)(num_intervals * (float)(my_rank+1)/p) + 1;
 
+    for(i = start; i < end; i++){
         x_middle = (i - 0.5) * rect_width;
-        area = sin(x_middle) * rect_width; 
-        sum = sum + area;
-    } 
+        area = sin(x_middle) * rect_width;
+        local_sum = local_sum + area;
+    }
 
-    printf("The total area is: %f\n", (float)sum);
+    MPI_Reduce(&local_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(my_rank == 0){
+        printf("The total area is: %f\n", (float)sum);
+    }
+
+    MPI_Finalize();
 
     return 0;
 }   
